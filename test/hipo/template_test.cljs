5d8e977afbc98fae0d4bb9cd3b709d8fe828b596
Julien Eluard <julien.eluard@gmail.com>
2014-11-13 15:30:11 -0300

    Removed template tests as this ns is not supposed to be used directly.

--- test/hipo/template_test.cljs
+++ test/hipo/template_test.cljs
@@ -1,111 +1,85 @@
 (ns hipo.template-test
-  (:require [cemerick.cljs.test :as test]
-            [hipo.template :as template])
+  (:require [cemerick.cljs.test :as test])
   (:require-macros [cemerick.cljs.test :refer [deftest is]]
                    [hipo.macros :refer [node]]))
 
 (deftest simple-template
-  (is (= "B" (-> [:b] template/node .-tagName)))
-  (is (= "some text" (-> "some text" template/node .-textContent)))
-  ;; unfortunately to satisfy the macro gods, you need to
-  ;; duplicate the vector literal to test compiled and runtime template
-  (let [e1 (template/node [:span "some text"])
-        e2 (node [:span "some text"])]
-    (doseq [e [e1 e2]]
-      (is (= "SPAN" (.-tagName e)))
-      (is (= "some text" (.-textContent e)))
-      (is (= js/document.TEXT_NODE (-> e .-childNodes (aget 0) .-nodeType)))
-      (is (zero? (-> e .-children .-length)))))
-  (let [e1 (template/node [:a {:href "http://somelink"} "anchor"])
-        e2 (node
-            [:a {:href "http://somelink"} "anchor"])]
-    (doseq [e [e1 e2]] (is (-> e .-tagName (= "A")))
-           (is (= "anchor" (.-textContent e)))
-           (is (= "http://somelink" (.getAttribute e "href")))))
+  (is (= "B" (.-tagName (node [:b]))))
+  (let [e (node [:span "some text"])]
+    (is (= "SPAN" (.-tagName e)))
+    (is (= "some text" (.-textContent e)))
+    (is (= js/document.TEXT_NODE (-> e .-childNodes (aget 0) .-nodeType)))
+    (is (zero? (-> e .-children .-length))))
+  (let [e (node [:a {:href "http://somelink"} "anchor"])]
+    (is (-> e .-tagName (= "A")))
+    (is (= "anchor" (.-textContent e)))
+    (is (= "http://somelink" (.getAttribute e "href"))))
   (let [a (atom 0)
         next-id #(swap! a inc)
         e (node [:span {:attr (next-id)}])]
     (is (= "1" (.getAttribute e "attr"))))
-  (let [e1 (template/node [:div#id {:class "class1 class2"}])
-        e2 (node [:div#id {:class "class1 class2"}])]
-    (doseq [e [e1 e2]]
-      (is (= "class1 class2" (.-className e)))))
-  (let [e (template/compound-element [:div (interpose [:br] (repeat 3 "test"))])]
+  (let [e (node [:div#id {:class "class1 class2"}])]
+    (is (= "class1 class2" (.-className e))))
+  (let [e (node [:div (interpose [:br] (repeat 3 "test"))])]
     (is (-> e .-outerHTML (= "<div>test<br>test<br>test</div>"))))
-  (let [e1 (template/compound-element [:div.class1 [:span#id1 "span1"] [:span#id2 "span2"]])
-        e2 (node [:div.class1 [:span#id1 "span1"] [:span#id2 "span2"]])]
-    (doseq [e [e1 e2]]
-      (is (= "span1span2" (.-textContent e)))
-      (is (= "class1" (.-className e)))
-      (is (= 2 (-> e .-childNodes .-length)))
-      (is (= "<span id=\"id1\">span1</span><span id=\"id2\">span2</span>"
-             (.-innerHTML e)))
-      (is (= "span1" (-> e .-childNodes (aget 0) .-innerHTML)))
-      (is (= "span2" (-> e .-childNodes (aget 1) .-innerHTML)))))
-  (is (= "<span id=\"id1\">span1</span><span id=\"id2\">span2</span>"
-         (-> [:div (for [x [1 2]] [:span {:id (str "id" x)} (str "span" x)])]
-             template/node
-             .-innerHTML))))
+  (let [e (node [:div.class1 [:span#id1 "span1"] [:span#id2 "span2"]])]
+    (is (= "span1span2" (.-textContent e)))
+    (is (= "class1" (.-className e)))
+    (is (= 2 (-> e .-childNodes .-length)))
+    (is (= "<span id=\"id1\">span1</span><span id=\"id2\">span2</span>"
+           (.-innerHTML e)))
+    (is (= "span1" (-> e .-childNodes (aget 0) .-innerHTML)))
+    (is (= "span2" (-> e .-childNodes (aget 1) .-innerHTML))))
+  (let [e (node[:div (for [x [1 2]] [:span {:id (str "id" x)} (str "span" x)])] )]
+    (is (= "<span id=\"id1\">span1</span><span id=\"id2\">span2</span>" (.-innerHTML e)))))
 
-(deftest attrs-template-test
+(deftest attrs-template
   (let [e (node [:a ^:attrs (merge {} {:href "http://somelink"}) "anchor"])]
     (is (-> e .-tagName (= "A")))
     (is (= "anchor" (.-textContent e)))
     (is (= "http://somelink" (.getAttribute e "href")))))
 
-(deftest nested-template-test
+(deftest nested-template
   ;; test html for example list form
   ;; note: if practice you can write the direct form (without the list) you should.
   (let [spans (for [i (range 2)] [:span (str "span" i)])
         end [:span.end "end"]
         h   [:div#id1.class1 (list spans end)]
-        e1 (template/compound-element h)
-        e2 (template/node             h)]
-    (doseq [e [e1 e2]]
-      (is (-> e .-textContent (= "span0span1end")))
-      (is (-> e .-className (= "class1")))
-      (is (-> e .-childNodes .-length (= 3)))
-      (is (-> e .-innerHTML
-              (= "<span>span0</span><span>span1</span><span class=\"end\">end</span>")))
-      (is (-> e .-childNodes (aget 0) .-innerHTML (= "span0")))
-      (is (-> e .-childNodes (aget 1) .-innerHTML (= "span1")))
-      (is (-> e .-childNodes (aget 2) .-innerHTML (= "end")))))
+        e (node h)]
+    (is (-> e .-textContent (= "span0span1end")))
+    (is (-> e .-className (= "class1")))
+    (is (-> e .-childNodes .-length (= 3)))
+    (is (-> e .-innerHTML
+            (= "<span>span0</span><span>span1</span><span class=\"end\">end</span>")))
+    (is (-> e .-childNodes (aget 0) .-innerHTML (= "span0")))
+    (is (-> e .-childNodes (aget 1) .-innerHTML (= "span1")))
+    (is (-> e .-childNodes (aget 2) .-innerHTML (= "end"))))
 
   ;; test equivalence of "direct inline" and list forms
   (let [spans (for [i (range 2)] [:span (str "span" i)])
         end   [:span.end "end"]
         h1    [:div.class1 (list spans end)]
         h2    [:div.class1 spans end]
-        e11 (template/compound-element h1)
-        e12 (template/node             h1)
-        e21 (template/compound-element h2)
-        e22 (template/node             h2)]
-    (doseq [[e1 e2] [[e11 e12]
-                     [e12 e21]
-                     [e21 e22]
-                     [e22 e11]]]
-      (is (= (.-innerHTML e1) (.-innerHTML e2))))))
+        e1 (node h1)
+        e2 (node h2)]
+    (is (= (.-innerHTML e1) (.-innerHTML e2)))))
 
 (deftest boolean-attribute
-  (let [e1 (template/node [:option {:selected true} "some text"])
-        e1c (node [:option {:selected true} "some text"])
-        e2 (template/node [:option {:selected false} "some text"])
-        e2c (node [:option {:selected false} "some text"])
-        e3 (template/node [:option {:selected nil} "some text"])
-        e3c (node [:option {:selected nil} "some text"])]
-    (doseq [e [e1 e1c]] (is (-> e (.getAttribute "selected") (= "true"))))
-    (doseq [e [e2 e2c]] (is (-> e (.getAttribute "selected") (nil?))))
-    (doseq [e [e3 e3c]] (is (-> e (.getAttribute "selected") (nil?))))))
+  (let [e1 (node [:option {:selected true} "some text"])
+        e2 (node [:option {:selected false} "some text"])
+        e3 (node [:option {:selected nil} "some text"])]
+    (is (= "true" (.getAttribute e1 "selected")))
+    (is (nil? (.getAttribute e2 "selected")))
+    (is (nil? (.getAttribute e3 "selected")))))
 
-(deftest custom-elements-test
+(deftest custom-elements
   (is (exists? (.-registerElement js/document)))
   (.registerElement js/document "my-custom-div" #js {:prototype (js/Object.create (.-prototype js/HTMLDivElement) #js {:test #js {:get (fn[] "")}}) :extends "div"})
-  (let [e1 (node [:div {:is "my-custom-div"} "content"])
-        e2 (template/node [:div {:is "my-custom-div"} "content"])]
-    (doseq [e [e1 e2]] (is (exists? (.-test e))))
-    (doseq [e [e1 e2]] (is (-> e .-tagName (= "DIV"))))
-    (doseq [e [e1 e2]] (is (= "content" (.-textContent e))))
-    (doseq [e [e1 e2]] (is (= "my-custom-div" (.getAttribute e "is"))))))
+  (let [e (node [:div {:is "my-custom-div"} "content"])]
+    (is (exists? (.-test e)))
+    (is (-> e .-tagName (= "DIV")))
+    (is (= "content" (.-textContent e)))
+    (is (= "my-custom-div" (.getAttribute e "is")))))
 
 (deftest namespaces
   (is (= "http://www.w3.org/1999/xhtml" (.-namespaceURI (node [:p]))))
