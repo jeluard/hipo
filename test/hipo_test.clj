5f840d0eb95987120faa2162134e6a72a58089d3
Julien Eluard <julien.eluard@gmail.com>
2014-12-07 18:39:17 -0300

    Play nice with the macro gods.

--- test/hipo_test.clj
+++ test/hipo_test.clj
@@ -1,6 +1,8 @@
 (ns hipo.hipo-test
   (:require [hipo.compiler :refer [compile-set-attribute!]]))
 
+; defmacro
+
 (defmethod compile-set-attribute! "test2"
   [[el a v]]
   `(.setAttribute ~el ~a ~(* 2 v)))
