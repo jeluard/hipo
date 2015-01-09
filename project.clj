a0c5706cee1a58db0b8fc18c639175e6e2a54eb3
Julien Eluard <julien.eluard@gmail.com>
2015-01-09 17:04:51 -0300

    Upgraded dependencies.

--- project.clj
+++ project.clj
@@ -4,11 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
-  :source-paths  ["src" "test"]
+  ;:source-paths  ["src" "test"]
   :profiles
   {:dev
-   {:dependencies [[org.clojure/clojurescript "0.0-2511"]]
-    :plugins [[lein-cljsbuild "1.0.3"]
+   {:dependencies [[org.clojure/clojurescript "0.0-2665"]]
+    :plugins [[lein-cljsbuild "1.0.4"]
               [com.cemerick/clojurescript.test "0.3.3"]]}}
   :cljsbuild
   {:builds
@@ -16,8 +16,8 @@
            :compiler {:output-to "target/unit-test.js"
                       :optimizations :whitespace
                       :pretty-print true}}}
-   :test-commands {"unit" ["slimerjs" :runner "dev-resources/document-register-element.js" "target/unit-test.js"]}}
-  :aliases {"clean-test" ["do" "clean," "cljsbuild" "clean," "cljsbuild" "test"]
-            "clean-install" ["do" "clean," "cljsbuild" "clean," "install"]}
+   :test-commands {"unit" ["slimerjs" :runner "dev-resources/document-register-element-0.1.2.js" "target/unit-test.js"]}}
+  :aliases {"clean-test" ["do" "clean," "cljsbuild" "test"]
+            "clean-install" ["do" "clean," "install"]}
   :deploy-repositories {"clojars" {:sign-releases false :url "https://clojars.org/repo/"}}
   :min-lein-version "2.5.0")
