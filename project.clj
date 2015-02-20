9ce96547df887b10135a74d6f5d6986fd935fb52
Julien Eluard <julien.eluard@gmail.com>
2015-02-20 14:19:07 -0300

    Upgraded dependencies.

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :profiles
   {:dev
    {:dependencies [[org.clojure/clojurescript "0.0-2850"]]
-    :plugins [[lein-cljsbuild "1.0.4"]
+    :plugins [[lein-cljsbuild "1.0.5"]
               [com.cemerick/clojurescript.test "0.3.3"]]}}
   :cljsbuild
   {:builds
@@ -15,7 +15,8 @@
            :compiler {:output-to "target/unit-test.js"
                       :optimizations :whitespace
                       :pretty-print true}}}
-   :test-commands {"unit" ["slimerjs" :runner "dev-resources/document-register-element-0.1.6.js" "target/unit-test.js"]}}
+   :test-commands {"phantomjs" ["phantomjs" :runner "dev-resources/document-register-element-0.1.6.js" "target/unit-test.js"]
+                   "slimerjs" ["slimerjs" :runner "dev-resources/document-register-element-0.1.6.js" "target/unit-test.js"]}}
   :aliases {"clean-test" ["do" "clean," "cljsbuild" "test"]
             "clean-install" ["do" "clean," "install"]}
   :deploy-repositories {"clojars" {:sign-releases false :url "https://clojars.org/repo/"}}
