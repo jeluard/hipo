f7508fac84af8b55ee318f08c6b18508e37e5102
Julien Eluard <julien.eluard@gmail.com>
2015-06-14 10:17:33 -0300

    Allow to force interpretation.

--- src/hipo/core.clj
+++ src/hipo/core.clj
@@ -3,20 +3,18 @@
             [hipo.interceptor :refer [intercept]]))
 
 (defmacro create-static
-  "Create a DOM element from hiccup style representation.
-
-   When 1 argument is provided it must be an hiccup vector. Invocation will return a DOM element.
-   When 2 arguments are provided the first argument must be a function of one argument returning an hiccup vector and the second argument a payload that will be provided to the function described previously.
-   Invocation will return a vector composed of a DOM element and an update function that can be used to reconciliate the DOM element based on a new payload."
+  "Create a DOM element from hiccup style representation provided as a vector. Second argument is an optional map of options."
   [h & [m]]
   (if h
-    `(hc/compile-create ~h ~m)))
+    (if (:force-interpretation m)
+      `(hipo.interpreter/create ~h ~m)
+      `(hc/compile-create ~h ~m))))
 
 (defmacro create
-  "Create a DOM element from hiccup style representation.
+  "Create a DOM element from hiccup style representation provided as a couple function / initial values. Third argument is an optional map of options that is propagated to the reconciliate function.
 
-   When 1 argument is provided it must be an hiccup vector. Invocation will return a DOM element.
-   When 2 arguments are provided the first argument must be a function of one argument returning an hiccup vector and the second argument a payload that will be provided to the function described previously.
-   Invocation will return a vector composed of a DOM element and an update function that can be used to reconciliate the DOM element based on a new payload."
+   The first argument must be a function of one argument returning an hiccup vector and the second argument a payload that will be provided to the function described previously.
+   Invocation will return a vector composed of a DOM element and a function that can be used to reconciliate the DOM element based on a new payload.
+   This function accept as second argument an optional map of options."
   [f o & [m]]
   `(hc/compile-reconciliate ~f ~o ~m))
