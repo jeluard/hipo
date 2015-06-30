f4883ca9ff486be831103ebe814090e46664dc97
Julien Eluard <julien.eluard@gmail.com>
2015-06-30 13:48:32 -0300

    Upgraded to Clojure 1.7.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/jeluard/hipo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-RC2"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]]
   :profiles
   {:dev
    {:dependencies [[org.clojure/clojurescript "0.0-3308"]
