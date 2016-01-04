4de673971263124bef210e73eb1f01ec1074f901
Julien Eluard <julien.eluard@gmail.com>
2016-01-04 14:02:50 +0100

    Upgraded dependencies.

--- project.clj
+++ project.clj
@@ -6,18 +6,16 @@
   :dependencies [[org.clojure/clojure "1.7.0"]]
   :profiles
   {:dev
-   {:dependencies [[org.clojure/clojurescript "0.0-3308"]
-                   [cljsjs/document-register-element "0.4.3-0"]
-                   [org.clojure/test.check "0.7.0"]]
-    :plugins [[lein-cljsbuild "1.0.5"]
-              [lein-doo "0.1.4-SNAPSHOT"]]}}
+   {:dependencies [[org.clojure/clojurescript "1.7.170"]
+                   [cljsjs/document-register-element "0.5.3-0"]]
+    :plugins [[lein-cljsbuild "1.1.2"]
+              [lein-doo "0.1.6"]]}}
   :cljsbuild
   {:builds
    {:test {:source-paths ["src" "test"]
            :compiler {:output-to "target/unit-test.js"
                       :main 'hipo.runner
-                      :optimizations :whitespace
-                      :pretty-print true}}}}
+                      :optimizations :none}}}}
   :aliases {"clean-test" ["do" "clean," "test," "doo" "phantom" "test" "once"]
             "clean-install" ["do" "clean," "install"]}
   :deploy-repositories {"clojars" {:sign-releases false :url "https://clojars.org/repo/"}}
